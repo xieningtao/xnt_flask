f21f84bf2947dcc5a8510b410ff499f1991b7eed
xieningtao <389124248@qq.com>
2019-12-14 18:22:04 +0800

    修改路径

--- com/test/uploadImage.py
+++ com/test/uploadImage.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
 import json
+import getpass
 from flask import Flask, request,make_response
 
-UPLOAD_FOLDER = r'C:\Users\xieningtao\Desktop\temp'   # 上传路径
+UPLOAD_FOLDER_NATIVE = r'C:\Users\xieningtao\Desktop\temp'   # 上传路径
+UPLOAD_FOLDER_REMOTE = r'/home/facetime/workspace/pics'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])   # 允许上传的文件类型
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 def allowed_file(filename):
     # 验证上传的文件名是否符合要求，文件名必须带点并且符合允许上传的文件类型要求，两者都满足则返回 true
@@ -22,7 +23,13 @@ def upload_file():
         myRespnse.headers["content-type"] = "application/json"
         if file and allowed_file(file.filename):   # 如果文件存在并且符合要求则为 true
                 filename = file.filename
-                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))   # 保存文件
+                user_name = getpass.getuser()
+                upload_fold = ""
+                if("xieningtao" == user_name):
+                    upload_fold = UPLOAD_FOLDER_NATIVE
+                else:
+                     upload_fold = UPLOAD_FOLDER_REMOTE
+                file.save(os.path.join(upload_fold, filename))   # 保存文件
                 myRespnse.data = json.dumps({"code":"100","msg":"图片上传成功"})   # 返回保存成功的信息
         else:
             myRespnse.data =  json.dumps({"code":"-101","msg":"只支持png,jpg,jpeg格式图片"})
